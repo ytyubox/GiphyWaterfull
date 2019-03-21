17d7b04dd682b3bb0b8b5055884de7bdbe099d28
Tsungyu Yu <ytyubox@gmail.com>
2019-03-21 15:22:19 +0800

    [s] cut & paste

--- YuPracticeGIF/extension/Result.swift
+++ YuPracticeGIF/extension/Result.swift
@@ -5,14 +5,6 @@ enum Result<ResultType> {
   case error(Error)
 }
 
-extension String{
-  var searchURL:URL?{
-    guard let secapedTerm = self.addingPercentEncoding(withAllowedCharacters: .alphanumerics) else {return nil}
-    let URLString = HOST + Path.search.rawValue + "q=" + secapedTerm + "&api_key=" + APIKEY
-    return URL(string: URLString)
-  }
-
-}
 extension Data {
   var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
     guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
